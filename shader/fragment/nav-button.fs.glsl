66b7686d8c360310047f743bfa1cbfb5d583e40e
wolmibo <102993087+wolmibo@users.noreply.github.com>
2023-08-08 15:55:08 +0200

    Add outline to nav-button

--- shader/fragment/nav-button.fs.glsl
+++ shader/fragment/nav-button.fs.glsl
@@ -12,10 +12,11 @@ uniform float scaleX;
 
 void main() {
   float r = length(objCoord);
-  r = (1.f - smoothstep(0.95f, 1.0f, r));
+  float outline = smoothstep(0.90f, 0.95f, r);
+  r = (1.f - smoothstep(0.95f, 1.f, r));
 
   float x = 1.6f * (scaleX * objCoord.x + 0.2f);
   float y = 2.4f * objCoord.y;
   float t = clamp(step(0.f, x) - smoothstep(0.9f, 1.0, x + abs(y)), 0.f, 1.f);
-  fragColor = r * mix(colorBack, colorFront, t);
+  fragColor = r * mix(colorBack, colorFront, max(t, outline));
 }
