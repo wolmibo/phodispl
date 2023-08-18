6e9348ac7efe8d34bb6417c8bf19a5740fd44d30
wolmibo <102993087+wolmibo@users.noreply.github.com>
2023-08-18 13:34:27 +0200

    create-font.sh: Fix path

--- resources/icons/create-font.sh
+++ resources/icons/create-font.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-DIR=$(git rev-parse --show-toplevel)/icons
+DIR=$(git rev-parse --show-toplevel)/resources/icons
 
 fontforge -lang=ff -c 'Open($1); Generate($2)' $DIR/source.svg $DIR/icons-font.otf
