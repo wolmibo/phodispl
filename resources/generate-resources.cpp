c93b309505cc01424bba3293225252b736a620fb
wolmibo <102993087+wolmibo@users.noreply.github.com>
2024-03-18 16:02:34 +0100

    Cleanup main in generate-resources.cpp

--- resources/generate-resources.cpp
+++ resources/generate-resources.cpp
@@ -56,6 +56,49 @@ namespace {
         "[[nodiscard]] std::span<const std::byte> {0}_data();\n",
     id);
   }
+
+
+
+  void generate_resources(
+      const std::filesystem::path& header_path,
+      const std::filesystem::path& source_path,
+      std::span<const char*> names,
+      std::span<const char*> files
+  ) {
+    if (names.size() != files.size()) {
+      throw std::runtime_error{"mismatch between input files and names"};
+    }
+
+    std::ofstream source(source_path);
+    if (!source) {
+      throw std::runtime_error{std::string{"unable to write to "} + source_path.string()};
+    }
+
+    source << "#include " << header_path.filename() << "\n";
+    source << "#include <array>\n";
+
+
+
+    std::ofstream header(header_path);
+    if (!header) {
+      throw std::runtime_error{std::string{"unable to write to "} + header_path.string()};
+    }
+
+    header << "// This file is generated during build. DO NOT EDIT.\n";
+    header << "#pragma once\n";
+    header << "#include <span>\n";
+    header << "#include <string_view>\n";
+    header << "namespace resources {\n";
+
+
+
+    for (size_t i = 0; i < names.size(); ++i) {
+      header << declare_file(names[i]);
+      source << define_file(files[i], names[i]);
+    }
+
+    header << "}\n";
+  }
 }
 
 
@@ -74,41 +117,10 @@ int main(int argc, const char* argv[]) {
   std::span names = args.subspan(3, (args.size() - 3) / 2);
   std::span files = args.subspan(3 + names.size());
 
-  if (names.size() != files.size()) {
-    throw std::runtime_error{"mismatch between input files and names"};
+  try {
+    generate_resources(args[1], args[2], names, files);
+  } catch (const std::exception& ex) {
+    std::cout << ex.what() << '\n';
+    return 2;
   }
-
-
-
-
-
-  std::ofstream source(args[2]);
-  if (!source) {
-    throw std::runtime_error{std::string{"unable to write to "} + args[2]};
-  }
-
-  source << "#include " << std::filesystem::path{args[1]}.filename() << "\n";
-  source << "#include <array>\n";
-
-
-
-  std::ofstream header(args[1]);
-  if (!header) {
-    throw std::runtime_error{std::string{"unable to write to "} + args[1]};
-  }
-
-  header << "// This file is generated during build. DO NOT EDIT.\n";
-  header << "#pragma once\n";
-  header << "#include <span>\n";
-  header << "#include <string_view>\n";
-  header << "namespace resources {\n";
-
-
-
-  for (size_t i = 0; i < names.size(); ++i) {
-    header << declare_file(names[i]);
-    source << define_file(files[i], names[i]);
-  }
-
-  header << "}\n";
 }
